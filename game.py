643adee47fdcad3efbbd361f7c4f86561accbcaa
espe-on <contact@josephcrawley.dev>
2020-10-06 17:55:24 +0100

    Snake now grows when snake eats food

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
-import pygame
 import time
+
+import pygame
+
 from game_objects.food import Food
 from game_objects.game_settings import GameSettings
 from game_objects.snake import Snake
@@ -9,14 +11,14 @@ from utils.game_utils import snake_is_out_of_bounds
 def game_loop(game_board: pygame, display, game_settings: GameSettings):
     display_size = game_settings.display_size
     pixel = game_settings.pixel_size
-    colors = game_settings.colours;
+    colors = game_settings.colours
     font_style_large = game_board.font.SysFont(None, 50)
-    font_style_small=  game_board.font.SysFont(None, 25)
+    font_style_small = game_board.font.SysFont(None, 25)
 
     snake = Snake(game_settings.starting_position, pixel)
     food = Food(display_size, pixel)
     clock = game_board.time.Clock()
-    game_running = True;
+    game_running = True
     game_playing = True
 
     def message(msg, color, font):
@@ -25,51 +27,54 @@ def game_loop(game_board: pygame, display, game_settings: GameSettings):
 
     # This section is the game intro screen
     while game_running:
+        score = 0
         while game_playing:
             for event in game_board.event.get():
                 if event.type == game_board.QUIT:
-                    game_running = False;
+                    game_running = False
                 if event.type == game_board.KEYDOWN:
                     if event.key == game_board.K_LEFT:
-                        snake.move_left();
+                        snake.move_left()
                     elif event.key == game_board.K_RIGHT:
-                        snake.move_right();
+                        snake.move_right()
                     elif event.key == game_board.K_UP:
-                        snake.move_up();
+                        snake.move_up()
                     elif event.key == game_board.K_DOWN:
-                        snake.move_down();
-            snake.resolve_position();
-            if snake_is_out_of_bounds(snake.head_position, display_size):
-                game_playing = False;
+                        snake.move_down()
             if food.position == snake.head_position:
-                score =+ 1
+                score = score + 1
                 food.move_food()
+            snake.resolve_position(score)
+            if snake_is_out_of_bounds(snake.head_position, display_size):
+                game_playing = False
 
             # Logging
-            print(f"Snake Position: x={snake.head_position[0]}, y={snake.head_position[1]}");
-            print(f"Food Position: x={food.position[0]}, y={food.position[1]}");
+            print(f"Snake Position: x={snake.head_position[0]}, y={snake.head_position[1]}")
+            print(f"Food Position: x={food.position[0]}, y={food.position[1]}")
+            print(f"Score: {score}")
             if food.position == snake.head_position:
-                print("Snake is on food!");
+                print("Snake is on food!")
             if snake_is_out_of_bounds(snake.head_position, display_size):
-                print("Snake out of bounds:");
+                print("Snake out of bounds:")
 
             # Rendering Game
-            display.fill(colors.background);
-            game_board.draw.rect(display, colors.highlight, [food.position[0], food.position[1], pixel, pixel]);
-            game_board.draw.rect(display, colors.main, [snake.head_position[0], snake.head_position[1], pixel, pixel]);
-            game_board.display.update();
-            clock.tick(10);
-
+            display.fill(colors.background)
+            game_board.draw.rect(display, colors.highlight, [food.position[0], food.position[1], pixel, pixel])
+            game_board.draw.rect(display, colors.main, [snake.head_position[0], snake.head_position[1], pixel, pixel])
+            for tail_position in snake.tail:
+                game_board.draw.rect(display, colors.main, [tail_position[0], tail_position[1], pixel, pixel])
+            game_board.display.update()
+            clock.tick(10)
 
         # Rendering Lost Screen
-        message("You Lost", colors.highlight, font_style_large);
-        game_board.display.update();
-        time.sleep(2);
+        message("You Lost", colors.highlight, font_style_large)
+        game_board.display.update()
+        time.sleep(2)
         display.fill(colors.background)
-        message(f"Score: {score}", colors.highlight, font_style_small);
-        game_board.display.update();
-        time.sleep(2);
-        game_running = False;
+        message(f"Score: {score}", colors.highlight, font_style_small)
+        game_board.display.update()
+        time.sleep(2)
+        game_running = False
 
-    game_board.quit();
-    quit();
+    game_board.quit()
+    quit()
--- game_objects/snake.py
+++ game_objects/snake.py
@@ -1,6 +1,7 @@
 class Snake:
     def __init__(self, position: [int], pixel):
         self.head_position = position
+        self.tail = []
         self.direction = [0, 0]
         self._pixel = pixel
 
@@ -8,14 +9,17 @@ class Snake:
         self.direction = [-self._pixel, 0];
 
     def move_right(self):
-        self.direction =  [+self._pixel, 0];
+        self.direction = [+self._pixel, 0];
 
     def move_up(self):
-        self.direction =  [0, -self._pixel];
+        self.direction = [0, -self._pixel];
 
     def move_down(self):
         self.direction = [0, +self._pixel];
 
-    def resolve_position(self):
+    def resolve_position(self, score):
+        self.tail.append(self.head_position)
+        if len(self.tail) > score:
+            self.tail.pop(0)
         self.head_position = [position + direction for position, direction in
-                         zip(self.position, self.direction)];
+                              zip(self.head_position, self.direction)];
