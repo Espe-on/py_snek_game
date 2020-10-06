15d5f0ee06c8b164dde4fcbf4a8cdc58a9c93f73
espe-on <contact@josephcrawley.dev>
2020-10-06 12:27:11 +0100

    rewrote game_settings as class GameSettings

--- game_settings.py
+++ game_settings.py
@@ -1,10 +1,25 @@
-board_width = 400
-board_height = 300
-pixel_width = 10
-pixel_height = 10
+class GameColors:
+    def __init__(self,
+                 main:tuple=(255,0,0),
+                 background:tuple=(0,0,0),
+                 highlight:tuple=(255, 0, 255)
+                 ):
+        self.main = main;
+        self.background = background;
+        self.color_highlight = highlight;
 
-colors = {
-    "main": (255, 0, 0),
-    "background": (0, 0, 0),
-    "highlight": (255, 255, 255)
-}
+class GameSettings:
+    def __init__(self,
+                 display_width:int=300,
+                 display_height:int=300,
+                 pixel_size:int=10,
+                 colors:GameColors=GameColors()
+                 ):
+        self.display_width=display_width;
+        self.display_height=display_height;
+        self.starting_position= (int(display_width/2), int(display_height/2));
+        self.pixel_size = pixel_size;
+        self.colours = colors
+
+
+
