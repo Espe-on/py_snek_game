fb4e1a4860446920a5b30731a059a6bef477ae35
espe-on <contact@josephcrawley.dev>
2020-10-06 10:41:52 +0100

    broke out game values into separate file

---
+++ game_settings.py
@@ -0,0 +1,10 @@
+board_width = 400
+board_height = 300
+pixel_width = 10
+pixel_height = 10
+
+colors = {
+    "main": (255, 0, 0),
+    "background": (0, 0, 0),
+    "highlight": (255, 255, 255)
+}
