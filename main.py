99476200d0bed40b04bf76f47d4c1550ba9edd05
espe-on <contact@josephcrawley.dev>
2020-10-06 10:42:15 +0100

    drew start of snake on board

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pygame as game_board
+import pygame as game_board;
+from game_settings import colors, board_width, board_height, pixel_width, pixel_height;
 
-game_board.init()
-display = game_board.display.set_mode((400, 300))
-game_board.display.update()
-game_board.display.set_caption("Py_Snek_Game")
+game_board.init();
+display = game_board.display.set_mode((board_width, board_height));
+game_board.display.update();
+game_board.display.set_caption("Py_Snek_Game");
 
-game_running = True
+game_running = True;
 
 while game_running:
     for event in game_board.event.get():
         if event.type == game_board.QUIT:
-            game_running = False
-        print(event)
+            game_running = False;
+        game_board.draw.rect(display, colors["main"], [int(board_width/2), int(board_height/2), pixel_width, pixel_height]);
+        game_board.display.update();
+        print(event);
 
-game_board.quit()
-quit()
+game_board.quit();
+quit();
