f2cb00a4adc1e0bb68d9c59f450a05ec76b95078
espe-on <contact@josephcrawley.dev>
2020-10-06 12:28:05 +0100

    game now runs as loop in main

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import pygame as game_board;
-from game_settings import colors, board_width, board_height, pixel_width, pixel_height;
+from game_settings import GameSettings
+from game import game_loop
 
 game_board.init();
-display = game_board.display.set_mode((board_width, board_height));
-game_board.display.update();
+game_settings = GameSettings();
+display = game_board.display.set_mode((game_settings.display_width, game_settings.display_height));
 game_board.display.set_caption("Py_Snek_Game");
 
-game_running = True;
-
-while game_running:
-    for event in game_board.event.get():
-        if event.type == game_board.QUIT:
-            game_running = False;
-        game_board.draw.rect(display, colors["main"], [int(board_width/2), int(board_height/2), pixel_width, pixel_height]);
-        game_board.display.update();
-        print(event);
-
-game_board.quit();
-quit();
+game_loop(game_board, display, game_settings);
