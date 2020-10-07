f4a0f7e312b3b013c43523d728a67a7b5871adbe
James Cleveland <jc@blit.cc>
2020-10-07 18:49:23 +0100

    refactor: make more stuff tuples

--- game_objects/game_settings.py
+++ game_objects/game_settings.py
@@ -19,7 +19,7 @@ class GameSettings:
         pixel_size: int = 10,
         colors: GameColors = GameColors(),
     ):
-        self.display_size = [display_width, display_height]
-        self.starting_position = [int(display_width / 2), int(display_height / 2)]
+        self.display_size = (display_width, display_height)
+        self.starting_position = (int(display_width / 2), int(display_height / 2))
         self.pixel_size = pixel_size
         self.colours = colors
