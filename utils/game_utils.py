f404e41632bb9d5ac276b4d7d10379420be3a4cd
James Cleveland <jc@blit.cc>
2020-10-29 10:50:09 +0000

    fixed function

--- utils/game_utils.py
+++ utils/game_utils.py
@@ -1,19 +1,17 @@
-import random
+from random import randrange
 from game_types import Position, Size
 
 
 def snake_is_out_of_bounds(snake_position: Position, display_size: Size):
     (x, y) = snake_position
     (width, height) = display_size
-    if (0 < x < width) and (0 < y < height):
+    if (0 <= x < width) and (0 <= y < height):
         return False
     return True
 
 
-def generate_random_square(display_size: Size, square_size: int):
+def generate_random_square(display_size: Size, square_size: int) -> Position:
     return (
-        round(random.randrange(0, display_size[0] - square_size) / square_size)
-        * square_size,
-        round(random.randrange(0, display_size[1] - square_size) / square_size)
-        * square_size,
+        randrange(0, int(display_size[0] / square_size)) * square_size,
+        randrange(0, int(display_size[1] / square_size)) * square_size,
     )
